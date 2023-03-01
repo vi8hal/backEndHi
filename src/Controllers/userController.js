b003d438ed77ce29a8dc410e0c84dbf60beed46c
vi8hal <124885261+vi8hal@users.noreply.github.com>
2023-03-01 10:53:21 +0530

    Update userController.js

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -65,32 +65,6 @@ const loginUser = async function (req, res) {
     res.status(500).send({ status: false, message: err.message });
   }
 };
-// const logOut = async function (req, res) {
-//   try {
-//     const { email, password } = req.body;
 
-//     const userSchema = Joi.object({
-//       password: Joi.string().min(8).max(15).required(),
-//       email: Joi.string().email().required()
-//     });
-
-//     const validationResult = userSchema.validate(req.body);
-
-//     if (validationResult.error) {
-//       return res.status(400).send({ status: false, message: validationResult.error.details[0].message });
-//     }
-
-//     const user = await userModel.findOne({ email, password });
-
-//     if (!user) {
-//       res.status(400).send({ status: false, message: "Invalid username or password" });
-//     } else {
-//       const token = jwt.sign(user._id.toString(), "Hiclousia"); // should be kept in the env file
-//       res.status(200).send({ status: true, message: "User logged in successfully", token: token, data: user });
-//     }
-//   } catch (err) {
-//     res.status(500).send({ status: false, message: err.message });
-//   }
-// };
 
 module.exports = { register, loginUser };
